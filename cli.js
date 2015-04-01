e496801a6aaea9d4105b2e587aaf0f6fd2491698
Finn <derfinn@gmail.com>
2015-04-01 23:36:42 +0200

    add not connected error, styling

--- cli.js
+++ cli.js
@@ -10,26 +10,27 @@ var airport = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/C
 
 var data = {}
 
-
 spawn(airport, ['-I']).stdout
   .pipe(split())
   .on('data', function (line) {
+    // console.log(line)
     var info = line.trim().split(': ')
-    if(info.length === 2)
-    data[info[0]] = info[1]
+    if (info.length === 2) data[info[0]] = info[1]
   })
-  .on('end',function () {
+  .on('end', function () {
+    if (data.AirPort === 'Off') return console.error(chalk.red('Wifi off.'))
     var rssi = Number(data.agrCtlRSSI)
     var noise = Number(data.agrCtlNoise)
-    if(argv.raw) return console.log(data)
+    if (argv.raw) return console.log(data)
+    if (!data.SSID) return console.error(chalk.red('Not connected.'))
     console.log(data.SSID, data['link auth'] === 'none' ? '🔓' : '🔒')
-    bar('WiFi Signal:\t', 100 + rssi)
-    bar('WiFi Noise:\t', 100 + noise)
+    bar('WiFi Signal:\t', 100 + rssi, 'green')
+    bar('WiFi Noise:\t', 100 + noise, 'red')
   })
-  
-function bar(text, percent) {
-  var length = Math.floor(percent/5)
-  var bar = chalk.green('[' + Array(length).join('|'),  Array(20 - length).join(' ') + ']')
+
+function bar (text, percent, color) {
+  var length = Math.floor(percent / 5)
+  var bar = chalk[color]('[' + Array(length).join('|'), Array(20 - length).join(' ') + ']')
   console.log(text, bar, percent)
 }
 
